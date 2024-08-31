9bc258e0a1c2abc03dc8d9656f21621afba6efab
Dylan Yelenich <dmyelenich@gmail.com>
2024-08-31 17:07:59 -0500

    commit finished power play 2

--- Goal2/Source2.cpp
+++ Goal2/Source2.cpp
@@ -2,182 +2,276 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <algorithm>
+#include <fstream>
 
 struct Object
 {
 	std::string name{};
-	int strength{ 0 };
-	int health{ 0 };
+	double strength{ 0 };
+	double health{ 0 };
+	int id{};
 };
-
-//giving global access to engines, player, and monster vector
+//giving global access without using header or class files
 std::random_device seed;
 std::default_random_engine engine(seed());
-std::normal_distribution<double> healthDist(30, 5);
-std::normal_distribution<double> strDist(5.0, 1.0);
-std::uniform_int_distribution<int> monsterPartySize(1, 3);
-std::bernoulli_distribution monsterAttackChance(0.75);
-std::normal_distribution<double> monsterAttackDist(monster.strength, 2);
-std::normal_distribution<double> playerAttackDist(monster.strength, 2);
-
-
-
 Object player;
 Object monster;
-std::vector<Object> monsters(monsterPartySize(engine));
+std::vector<Object> monsters;
 
 void createPlayer();
-void createMonsters();
-int monsterAttack();
-int playerAttack();
-
+void displayMonsters();
+void loadMonsters();
+void displayBattle();
+double attack(const Object& attacker);
+double defend(Object& defender, int damage);
+double heal(Object& healer);
+void monsterAttack();
+void playerAttack();
+bool allMonstersDead();;
 
 int main()
 {
-	createPlayer();
-	createMonsters();
-
-	while (player.health > 0)
-	{
-		//print player
-		std::cout << player.name << " health: " << player.health << " strength: " << player.strength << std::endl;
-		std::cout << std::endl;
-		//print monsters
-		std::cout << "Monsters: " << std::endl;
-		for (auto& monster : monsters)
-		{
-			std::cout << monster.name << " health: " << monster.health << std::endl;
-		}
-		system("pause");
-		system("CLS");
-		//Player Action 
-		std::cout << std::endl;
-		std::cout << player.name << ", do you want to attack or heal? Enter 1 for attack and 2 for heal. " << std::endl;
-		int playerAction{};
-		std::cin >> playerAction;
-
-		//Attack Choice Tree
-		if (playerAction == 1)
-		{
-			std::cout << "Choose a monster to attack:" << std::endl;
-			for (auto& monster : monsters)
-			{
-				std::cout << monster.name << " health: " << monster.health << std::endl;
-			}
-			int playerAttackChoice;
-			std::cin >> playerAttackChoice;
-
-			//will throw out of bounds exception error -- need to handle this error in final program 
-			switch (playerAttackChoice)
-			{
-			case 1:
-			{
-				std::cout << "monster hit for: " << player.strength << " damage" << std::endl;
-				monsters.at(0).health = monsters.at(0).health - player.strength;
-				std::cout << monsters.at(0).name << " health: " << monsters.at(0).health << std::endl;
-				break;
-			}
-			case 2:
-			{
-				std::cout << "monster hit for: " << player.strength << " damage" << std::endl;
-				monsters.at(1).health = monsters.at(1).health - player.strength;
-				std::cout << monsters.at(1).name << " health: " << monsters.at(1).health << std::endl;
-				break;
-			}
-			case 3:
-			{
-				std::cout << "monster hit for: " << player.strength << " damage" << std::endl;
-				monsters.at(2).health = monsters.at(2).health - player.strength;
-				std::cout << monsters.at(2).name << " health: " << monsters.at(2).health << std::endl;
-				break;
-			}
-			}
-		}
-		//healing action 
-		if (playerAction == 2)
-		{
-			player.health = player.health + (player.strength * 2);
-			std::cout << "player health: " << player.health << std::endl;
-		}
-
-		system("pause");
-		system("CLS");
-
-		std::cout << "Monster's Turn!!! " << std::endl;
-		//monster attack
-		for (auto& monster : monsters)
-		{
-			//need to figure out dead monster not attacking
-			if (monsterAttackChance(engine))
-			{
-				std::cout << monsterAttackChance(engine) << " engine " << std::endl;
-				std::cout << monster.name << " attacks for : " << monsterAttack();
-				std::cout << std::endl;
-				player.health = player.health - monster.strength;
-				std::cout << player.name << " health: " << player.health << std::endl;
-			}
-			else
-			{
-				std::cout << "monster attacks and misses" << std::endl;
-			}
-		}
-	}
+	displayBattle();
 }
 
 void createPlayer()
 {
-	//Player Str
+	std::normal_distribution<double> healthDist(30.0, 5.0);
+	std::normal_distribution<double> strDist(5.0, 1.0);
 	player.name = "Trogdor";
-	player.strength = strDist(engine);
-	//error handling to make sure player strength > 1
-	if (player.strength < 1)
+	do
 	{
 		player.strength = strDist(engine);
 	}
-	//std::cout << "strength: " << player.strength << std::endl;
+	while (player.strength < 1.0);
 
-	//Player Health
-	player.health = healthDist(engine);
-	//error handling to ensure health > 1
-	if (player.health < 1)
+	do
 	{
 		player.health = healthDist(engine);
 	}
-	//std::cout << "health: " << player.health << std::endl;
+	while (player.health < 1.0);
 }
 
-void createMonsters()
+void displayMonsters()
 {
-	int monsterNumber{ 1 };
-	for (auto& monster : monsters)
+	std::cout << "Monsters: " << std::endl;
+	int monsterChoice{ 1 };
+	for (const auto& monster : monsters)
 	{
-		monster.name = "Monster " + std::to_string(monsterNumber++);
-		monster.health = healthDist(engine);
-		if (monster.health < 1)
+		if (monster.health <= 0)
 		{
-			monster.health = healthDist(engine);
+			std::cout << "<DEAD> x_x" << std::endl;
 		}
-		monster.strength = strDist(engine);
-		if (monster.strength < 1)
+		else
 		{
-			monster.strength = strDist(engine);
+			std::cout << monsterChoice++ << ". " << monster.name << "\n"
+				<< "health: " << monster.health << std::endl;
 		}
-		std::cout << monster.name << " " <<
-			"health: " << monster.health << " " <<
-			"strength: " << monster.strength << std::endl;
 	}
 }
 
-//Damage dealt on attacks are based on a normal distribution with the mean being the object’s strength and a standard deviation of 2.
-
-int monsterAttack(monsterAttackDist(engine)
+void loadMonsters()
 {
+	const std::string fileName("monsters.txt");
+	std::fstream fin;
+	fin.open(fileName);
+	if (!fin.is_open())
+	{
+		std::cout << fileName << " did not open correctly" << std::endl;
+	}
 
-	std::cout << "monster attack amt: " << std::endl;
-	return
+	int numOfMonstersInFile{};
+	fin >> numOfMonstersInFile;
+
+	//temporary vector to pull monsters from to put into the monster party vector
+	std::vector<Object> allMonsters;
+	for (int i{ 0 }; i < numOfMonstersInFile; i++)
+	{
+		Object monster;
+		fin >> monster.name;
+		fin >> monster.id;
+		fin >> monster.strength;
+		fin >> monster.health;
+		allMonsters.push_back(monster);
+	}
+
+	std::uniform_int_distribution<int> monsterPartySizeDist(1, 3);
+	int monsterPartySize = monsterPartySizeDist(engine);
+
+	//shuffling the vector of allMonsters around to randomize the encounter
+	std::shuffle(allMonsters.begin(), allMonsters.end(), engine);
+
+	//pushes back the monsters after the shuffle has taken place
+	for (int i{ 0 }; i < monsterPartySize; i++)
+	{
+		monsters.push_back(allMonsters[i]);
+	}
 }
 
-int playerAttack()
+// Damage dealt on attacks are based on a normal distribution with the mean being the object’s strength and a standard deviation of 2.
+void displayBattle()
 {
+	createPlayer();
+	loadMonsters();
+	std::cout << "Welcome to the Haunted Forest " << std::endl;
+	std::cout << "Be wary of Monsters!!!" << std::endl;
+	std::cout << player.name << " the brave warrior enters the forest..." << std::endl;
 
+	while (player.health > 0 && !allMonstersDead())
+	{
+		std::cout << player.name
+			<< " health: " << player.health
+			<< " strength: " << player.strength << std::endl;
+		std::cout << std::endl;
+
+		system("pause");
+		system("CLS");
+		//Player Action 
+		std::cout << std::endl;
+
+		std::cout << player.name << ", do you want to attack or heal? Enter 1 for attack and 2 for heal. " << std::endl;
+		std::cout << std::endl;
+
+		displayMonsters();
+
+		int playerAction{};
+		std::cin >> playerAction;
+		system("CLS");
+
+		if (playerAction == 1)
+		{
+			playerAttack();
+			
+		}
+		if (playerAction == 2)
+		{
+			heal(player);
+		}
+
+		system("pause");
+		system("CLS");
+
+		monsterAttack();
+	}
+
+	if (player.health <= 0)
+	{
+		std::cout << "You Have Died" << std::endl;
+	}
+	if (player.health <= 0 && allMonstersDead())
+	{
+		std::cout << "BUT" << std::endl;
+	}
+	if (allMonstersDead())
+	{
+		std::cout << "You have killed the monsters!!!" << std::endl;
+	}
+	system("PAUSE");
 }
+
+double attack(const Object& attacker)
+{
+	std::normal_distribution<double> attackDist(attacker.strength, 2);
+	double damage;
+	do
+	{
+		damage = round(attackDist(engine));
+	}
+	while (damage < 1.0);
+
+	return damage;
+}
+
+double defend(Object& defender, int damage)
+{
+	defender.health -= damage;
+	std::cout << damage << " damage to " << defender.name << "!!!" << std::endl;
+	if (defender.health < 0)
+	{
+		defender.health = 0;
+	}
+	return defender.health;
+}
+
+
+double heal(Object& healer)
+{
+	std::normal_distribution<double> healDist(healer.strength * 2, 3);
+	double healAmount;
+	do
+	{
+		healAmount = round(healDist(engine));
+	}
+	while (healAmount < 1.0);
+
+	healer.health += healAmount;
+	std::cout << healer.name << " heals for " << healAmount << " health!" << std::endl;
+	return healer.health;
+}
+
+void monsterAttack()
+{
+	if (!allMonstersDead())
+	{
+		std::cout << "Monster's Turn!!! " << std::endl;
+		//monster attack
+		std::bernoulli_distribution willAttack(0.75);
+		for (const auto& monster : monsters)
+		{
+			if (monster.health > 0)
+			{
+				if (willAttack(engine))
+				{
+					std::cout << monster.name << " attacks!" << std::endl;
+					defend(player, attack(monster));
+				}
+				else
+				{
+					std::cout << monster.name << " attacks and misses! Wiff! " << std::endl;
+				}
+			}
+		}
+	}
+}
+
+void playerAttack()
+{
+	if (!allMonstersDead())
+	{
+		std::cout << "Choose a monster to attack:" << std::endl;
+		displayMonsters();
+
+		int playerAttackChoice;
+		std::cin >> playerAttackChoice;
+		while (playerAttackChoice < 1 || playerAttackChoice > monsters.size())
+		{
+			std::cout << "please choose a vaild number. " << std::endl;
+			std::cin >> playerAttackChoice;
+		}
+
+		defend(monsters.at(playerAttackChoice - 1), attack(player));
+
+		if (monsters.at(playerAttackChoice - 1).health <= 0)
+		{
+			std::cout << monsters.at(playerAttackChoice - 1).name << " has been kilt ded! x_x" << std::endl;
+		}
+		else
+		{
+			std::cout << monsters.at(playerAttackChoice - 1).name << " health: " << monsters.at(playerAttackChoice - 1).health << std::endl;
+		}
+	}
+}
+
+bool allMonstersDead()
+{
+	for (const auto& monster : monsters)
+	{
+		if (monster.health > 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
